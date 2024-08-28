b8d37e36560ba394625563a4a16a8da9edeab158
Sam-WardPickering <samuel.wardpickering@gmail.com>
2024-08-28 22:52:58 +1200

    Create event model, set up mutations and queries for events

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const Event = require('./models/event');
+
+require("dotenv").config();
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -44,29 +45,41 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return events;
+                return Event.find()
+                    .then(events => {
+                        return events.map(event => {
+                            return {... event._doc}
+                        });
+                    })
+                    .catch(err => {
+                        throw err;
+                    });
             },
             createEvent: (args) => {
-                const event = {
-                    _id: Math.random().toString(),
+                const event = new Event({
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: args.eventInput.date
-                }
-                events.push(event);
-                return event;
+                    date: new Date(args.eventInput.date)
+                });
+                return event
+                    .save()
+                    .then(result => {
+                        console.log(result);
+                        return {...result._doc};    // get core properties from mongoose
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        throw err;
+                    });
             }
         },
         graphiql: true
     })
 );
 
-mongoose
-.connect(
-    `mongodb+srv://
-    ${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}
-    @cluster0.tsq6bnw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.tsq6bnw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`
 )
 .then(() => {
     app.listen(3000);
