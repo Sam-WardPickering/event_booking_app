64a9453661bd0b5d9c0a5684cc3782a089bef34d
Sam-WardPickering <samuel.wardpickering@gmail.com>
2024-09-04 22:28:15 +1200

    Link users and events

--- app.js
+++ app.js
@@ -74,13 +74,27 @@ app.use(
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: new Date(args.eventInput.date)
+                    date: new Date(args.eventInput.date),
+                    creator: '66d832abaabaeb6072c25663'
                 });
+
+                let createdEvent;
+
                 return event
                     .save()
                     .then(result => {
-                        console.log(result);
-                        return { ...result._doc, _id: result._doc._id.toString() };    // get core properties from mongoose
+                        createdEvent = { ...result._doc, _id: result._doc._id.toString() }
+                        return User.findById('66d832abaabaeb6072c25663');
+                    })
+                    .then(user => {
+                        if (!user) {
+                            throw new Error('User not found.');
+                        }
+                        user.createdEvents.push(event);
+                        return user.save();
+                    })
+                    .then(result => {
+                        return createdEvent;
                     })
                     .catch(err => {
                         console.log(err);
