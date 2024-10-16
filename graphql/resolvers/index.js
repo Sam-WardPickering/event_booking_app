4168bbe7b8f62ff9180723de6a38d8c0edf49201
Sam-WardPickering <samuel.wardpickering@gmail.com>
2024-10-16 23:02:59 +1300

    Create reusable function for bookings

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -15,6 +15,17 @@ const transformEvent = event => {
     };
 }
 
+const transformBooking = booking => {
+    return {
+        ...booking._doc, 
+        id: booking.id,
+        user: user.bind(this, booking._doc.user),
+        event: singleEvent.bind(this, booking._doc.event),
+        createdAt: dateToString(booking._doc.createdAt),
+        updatedAt: dateToString(booking._doc.updatedAt)
+    };
+}
+
 const events = async eventIds => {
     try {
         const events = await Event.find({ _id: { $in: eventIds } })
@@ -65,14 +76,7 @@ module.exports = {
         try {
             const bookings = await Booking.find();
             return bookings.map(booking => {
-                return { 
-                    ...booking._doc, 
-                    id: booking.id,
-                    user: user.bind(this, booking._doc.user),
-                    event: singleEvent.bind(this, booking._doc.event),
-                    createdAt: dateToString(booking._doc.createdAt),
-                    updatedAt: dateToString(booking._doc.updatedAt)
-                }
+                return transformBooking(booking);
             })
         } catch (err) {
             throw err;
@@ -132,14 +136,7 @@ module.exports = {
             event: fetchedEvent
         });
         const result = await booking.save();
-        return {
-            ...result._doc,
-            _id: result.id,
-            user: user.bind(this, booking._doc.user),
-            event: singleEvent.bind(this, booking._doc.event),
-            createdAt: dateToString(result._doc.createdAt),
-            updatedAt: dateToString(result._doc.updatedAt)
-        };
+        return transformBooking(result);
     },
     cancelBooking: async args => {
         try {   
