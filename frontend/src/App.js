ff330db626d196154b3178240e8b6f74acd30552
Sam-WardPickering <samuel.wardpickering@gmail.com>
2024-10-27 13:55:21 +1300

    Add routing, create auth page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/events" component={null} />
+        <Route path="/bookings" component={null} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
