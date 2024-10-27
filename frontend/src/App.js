9adba0e1af5f6e6a91e3708cec7487408b98627f
Sam-WardPickering <samuel.wardpickering@gmail.com>
2024-10-27 14:01:38 +1300

    Add events, bookings pages and add to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
+import EventsPage from './pages/Events';
+import BookingsPage from './pages/Bookings';
 
 function App() {
   return (
@@ -9,8 +11,8 @@ function App() {
       <Switch>
         <Redirect from="/" to="/auth" exact />
         <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={null} />
-        <Route path="/bookings" component={null} />
+        <Route path="/events" component={EventsPage} />
+        <Route path="/bookings" component={BookingsPage} />
       </Switch>
     </BrowserRouter>
   );
