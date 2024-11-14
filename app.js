3c272c360db1e1a626aa9ab60b73c0cfd09099ec
Sam-WardPickering <samuel.wardpickering@gmail.com>
2024-11-14 23:25:42 +1300

    Link API for login & signup

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ require("dotenv").config();
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -28,7 +40,7 @@ mongoose.connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.tsq6bnw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`
 )
 .then(() => {
-    app.listen(3000);
+    app.listen(8000);
 })
 .catch(err => {
     console.log(err);
