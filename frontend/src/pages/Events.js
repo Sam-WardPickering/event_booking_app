0db50cc4c359feb5a4b449cd2ecc1894850dbcb7
Sam-WardPickering <samuel.wardpickering@gmail.com>
2024-11-20 23:06:19 +1300

    Add initial styles for events page && create modal file and corresponding css

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import './Events.css';
 
 class EventsPage extends Component {
     render() {
-        return <h1>Events Page</h1>;
+        return <div className="events-control">
+            <p>Share your own events!</p>
+            <button className="btn">Create Event</button>
+        </div>;
     }
 }
 
