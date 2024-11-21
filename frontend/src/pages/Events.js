a2a864559b42cb85f57cdbcaaa1a5f8e4c12ca6a
Sam-WardPickering <samuel.wardpickering@gmail.com>
2024-11-21 20:47:56 +1300

    Display modal on create event button press

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -4,16 +4,30 @@ import './Events.css';
 import Backdrop from '../components/Backdrop/Backdrop';
 
 class EventsPage extends Component {
+
+    state = {
+        creating: false
+    };
+
+    startCreateEventHandler = () => {
+        this.setState({creating: true});
+    }
+
+
     render() {
         return (
             <React.Fragment>
-                <Backdrop />
-                <Modal title="Add Event" canCancel canConfirm>
-                    <p>Modal Content</p>
-                </Modal>
+                {this.state.creating && <Backdrop />}
+                {this.state.creating && (
+                    <Modal title="Add Event" canCancel canConfirm>
+                        <p>Modal Content</p>
+                    </Modal>
+                )}
                 <div className="events-control">
                 <p>Share your own events!</p>
-                <button className="btn">Create Event</button>
+                <button className="btn" onClick={this.startCreateEventHandler}>
+                    Create Event
+                </button>
             </div>
             </React.Fragment>
         );
