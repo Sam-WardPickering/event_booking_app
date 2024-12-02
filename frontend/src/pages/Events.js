ab21694a37c44d23c1bc5633568ef23d7f28df26
Sam-WardPickering <samuel.wardpickering@gmail.com>
2024-12-02 22:33:01 +1300

    Store create event input values & add basic field validation

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -9,12 +9,34 @@ class EventsPage extends Component {
         creating: false
     };
 
+    constructor(props) {
+        super(props);
+        this.titleElRef = React.createRef();
+        this.priceElRef = React.createRef();
+        this.dateElRef = React.createRef();
+        this.descriptionElRef = React.createRef();
+    };
+    
     startCreateEventHandler = () => {
         this.setState({creating: true});
-    }
+    };
 
     modalConfirmHandler = () => {
         this.setState({creating: false});
+
+        const title = this.titleElRef.current.value;
+        const price = +this.priceElRef.current.value;
+        const date = this.dateElRef.current.value;
+        const description = this.descriptionElRef.current.value;
+
+        if (title.trim().length === 0 || price.trim().length === 0 || date.trim().length === 0 || description.trim().length === 0) {
+            console.error("Please fill out all the required fields");
+            return;
+        }
+
+        const event = {title, price, date, description};
+        console.log(event);
+        
     };
 
     modalCancelHandler = () => {
@@ -31,19 +53,19 @@ class EventsPage extends Component {
                         <form>
                             <div className="form-control">
                                 <label htmlFor="title">Title</label>
-                                <input type="text" id="title"></input>
+                                <input type="text" id="title" ref={this.titleElRef} />
                             </div>
                             <div className="form-control">
                                 <label htmlFor="price">Price</label>
-                                <input type="number" id="price"></input>
+                                <input type="number" id="price" ref={this.priceElRef} />
                             </div>
                             <div className="form-control">
                                 <label htmlFor="date">Date</label>
-                                <input type="date" id="date"></input>
+                                <input type="datetime-local" id="date" ref={this.dateElRef} />
                             </div>
                             <div className="form-control">
                                 <label htmlFor="description">Description</label>
-                                <textarea id="description" rows="4" />
+                                <textarea id="description" rows="4" ref={this.descriptionElRef} />
                             </div>
                         </form>
                     </Modal>
